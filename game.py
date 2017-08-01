10cb17543b89c018cce7f2db82781363e1b14c14
federico <fede.larregle@gmail.com>
2017-07-31 23:57:25 -0300

    Adding a little bit of movement

--- game.py
+++ game.py
@@ -12,7 +12,11 @@ class Game:
 	def __init__(self, player):
 		pygame.init()
 		self.player = player
-	
+		self.key_up = False
+		self.key_down = False
+		self.key_left = False
+		self.key_right = False
+
 
 	def run(self):
 		done = False
@@ -25,6 +29,30 @@ class Game:
 				if event.type == pygame.QUIT:
 					pygame.quit()
 					sys.exit()
+
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_DOWN:
+						self.key_down = True
+						self.player.move_down()
+					if event.key == pygame.K_UP:
+						self.key_up = True
+						self.player.move_up()
+					if event.key == pygame.K_RIGHT:
+						self.key_right = True
+						self.player.move_right()
+					if event.key == pygame.K_LEFT:
+						self.key_left = True
+						self.player.move_left()
+				elif event.type == pygame.KEYUP:
+					if event.key == pygame.K_DOWN:
+						self.key_down = False
+					if event.key == pygame.K_UP:
+						self.key_up = False
+					if event.key == pygame.K_LEFT:
+						self.key_left = False
+					if event.key == pygame.K_RIGHT:
+						self.key_right = False
+
 				main_surface.fill(self.BACKGROUND_COLOR)
 				pygame.draw.rect(main_surface, self.player.color, (self.player.position.x, self.player.position.y, 25, 25))
 				pygame.display.flip()
--- player.py
+++ player.py
@@ -1,5 +1,24 @@
+from vector import Vector
+
 class Player:
 
 	def __init__(self, start_position):
 		self.position = start_position
 		self.color = (255, 255, 255)
+
+	def move_down(self):
+		v2 = Vector(0, 5)
+		self.position.add(v2)
+
+	def move_up(self):
+		v2 = Vector(0, -5)
+		self.position.add(v2)
+
+	def move_right(self):
+		v2 = Vector(5, 0)
+		self.position.add(v2)
+
+	def move_left(self):
+		v2 = Vector(-5, 0)
+		self.position.add(v2)
+
